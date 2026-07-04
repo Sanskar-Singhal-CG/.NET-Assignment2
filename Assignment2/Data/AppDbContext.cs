2ac646f15191d36e87c2d913609781fd527af3cc
Sanskar Singhal <Sanskar.Singhal@cginfinity.com>
2026-07-04 20:46:27 +0530

    TASK 3

---
+++ Assignment2/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Assignment2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,9 +1,15 @@
+using Assignment2.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Register the DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -23,3 +29,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+// Add-Migration InitialCreate
