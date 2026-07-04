e72b92fbd15cdcb02084c83b580e177005edf849
Sanskar Singhal <Sanskar.Singhal@cginfinity.com>
2026-07-04 20:37:40 +0530

    TASK 1

---
+++ Assignment2/Models/Product.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Category is required")]
+        public string Category { get; set; } = string.Empty;
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be greater than 0")]
+        public decimal Price { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Stock must be greater than or equal to 0")]
+        public int Stock { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+    }
+}
